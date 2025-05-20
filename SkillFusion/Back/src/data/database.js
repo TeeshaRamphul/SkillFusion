85ff0c012c6b283b8d105aa9ce10b43d4a85c581
TeeshaRamphul <teesha.ramphul@oclock.school>
2025-05-20 09:25:32 +0200

    empty master for merging dev

--- SkillFusion/Back/src/data/database.js
+++
@@ -1,8 +0,0 @@
-import "dotenv/config";
-import { Client } from "pg";
-
-const client = new Client(process.env.PG_URL);
-
-client.connect();
-
-export default client;
--- SkillFusion/jest.setup.js
+++
@@ -1,6 +0,0 @@
-import { config } from 'dotenv';
-import path from 'path';
-
-config({
-  path: path.resolve(process.cwd(), 'Back', '.env.test')
-});
