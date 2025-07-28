8ed387dad666708e0c43f8a38d4c5496ec13f05a
TeeshaRamphul <teesha.ramphul@oclock.school>
2025-07-29 00:52:56 +0200

    fix: enforce SSL from PG_URL only

--- SkillFusion/Back/src/models/connection.js
+++ SkillFusion/Back/src/models/connection.js
@@ -1,30 +1,7 @@
-/*import 'dotenv/config'; // Cette ligne DOIT être en haut et sans erreur
+import 'dotenv/config';
 import { Sequelize } from 'sequelize';
 
-const PG_URL = process.env.PG_URL || 'postgresql://skillfusion_api_1avj_user:S69eCdLVEgD2owU6XqfOSMKvd4Mu1IoA@dpg-d23vbrqdbo4c7389cen0-a.frankfurt-postgres.render.com/skillfusion_api_1avj';
-
-if (!PG_URL) {
-  throw new Error('PG_URL environment variable is required');
-}
-
-const sequelize = new Sequelize(PG_URL, {
-  dialect: 'postgres',
-  define: {
-    underscored: true,
-    timestamps: true,
-    createdAt: 'created_at',
-    updatedAt: 'updated_at',
-  },
-  logging: process.env.NODE_ENV === 'test' ? false : console.log,
-});
-
-export { sequelize }
-
-*/
-import 'dotenv/config'; // Cette ligne DOIT être en haut et sans erreur
-import { Sequelize } from 'sequelize';
-
-const PG_URL = process.env.PG_URL || 'postgresql://skillfusion_api_1avj_user:S69eCdLVEgD2owU6XqfOSMKvd4Mu1IoA@dpg-d23vbrqdbo4c7389cen0-a.frankfurt-postgres.render.com/skillfusion_api_1avj';
+const PG_URL = process.env.PG_URL;
 
 if (!PG_URL) {
   throw new Error('PG_URL environment variable is required');
@@ -42,7 +19,7 @@ const sequelize = new Sequelize(PG_URL, {
   dialectOptions: {
     ssl: {
       require: true,
-      rejectUnauthorized: false, // Pour accepter les certificats auto-signés
+      rejectUnauthorized: false,
     },
   },
 });
